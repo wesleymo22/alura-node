ced7647bf67cbe3103e5d3d5901e05695367759b
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-04-17 22:40:29 -0300

    Criando node e Servidor local

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http")
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Curso de node')
+})
+
+server.listen(port, () => {
+    console.log(`Servidor escutando na porta http://localhost:${port}`)
+})
+

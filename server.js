e4b62524583e9b84e2d651d07647bc815d6fa97d
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-04-17 22:53:19 -0300

    Criando rotas e nodemon

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Pagina de livros',
+    '/autores': 'Pagina de autores'
+}
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Curso de node')
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, () => {

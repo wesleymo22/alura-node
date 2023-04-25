017e3326b1c15b21cebe5d6560deb1602d6f0eff
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-04-24 21:31:06 -0300

    Evoluindo Rotas

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const livros = [
     { id: 1, "titulo": "Senhor dos Aneis" },
     { id: 2, "titulo": "O Hobbit"}
@@ -11,8 +13,28 @@ app.get('/', (req, res) => {
     res.status(200).send('Curso de Node')
 }) 
 
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    res.status(200).json(livros[index])
+})
+
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Criado com sucesso')
+})
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    livros[index].titulo = req.body.titulo
+    res.status(200).json(livros)
+})
+
+function buscaLivro(id){
+    return livros.findIndex((livro) => livro.id == id)
+}
 
 export default app
